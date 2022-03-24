14eb6ba005d02940cfcf9465fef82fb38210f5cf
Erem Şimşek <erem.simsek@kariyer.net>
2022-03-24 23:33:44 +0300

    feat: created folder operators

---
+++ operators/script.js
@@ -0,0 +1,84 @@
+// Matematiksel Operatörler
+// Toplama
+let x = 4;
+let y = 2;
+
+console.log(x + y); // 6
+
+// Çıkarma
+
+let z = 4;
+let t = 2;
+
+console.log(z - t); // 2
+
+// Çarpma 
+let e = 10;
+let f = 11;
+console.log(e * f); // 110
+
+
+// Bölme 
+
+let g = 20;
+let h = 5;
+console.log(g / h); // 4
+
+// Mod(Kalan)
+
+let k = 20;
+let l = 4;
+console.log(k % l); // 0
+
+// Arttırma Azaltma Operatörleri
+
+let a = 5;
+console.log(++a); // 6
+console.log(--a); // 5
+
+// Karşılaştırma Operatörleri
+
+/*
+> : Büyük
+< : Küçük
+>= : Büyük Eşit
+<= : Küçük Eşit
+== : Eşit
+!= : Eşit Değil
+=== : Tür ve Değer Eşit
+!== : Eşit Değil Tür ve Değer
+*/
+
+// Mantıksal Operatörleri
+
+/*
+& AND
+| OR
+^ XOR 
+~ NOT
+*/
+
+// Atama Operatörleri
+
+/*
+= : Sağdaki değeri sola aktarır.
+x = 1;
+*/
+
+// Bitsel Kaydırma Operatörleri
+
+/*
+>> : Sağa kaydır
+<< : Sola kaydır 
+*/
+
+// Örnek
+const n = 5;
+console.log(2 << n); // 64
+console.log(n >> 1); // 2
+
+// Koşullu Operatör
+
+/*
+kosul ? ifTrue : ifFalse;
+*/
